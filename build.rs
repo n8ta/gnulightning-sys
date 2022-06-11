4f4f11b628de98bd2e64485f8fc2a776207c37f6
Nathaniel Tracy-Amoroso <natetracyamoroso@gmail.com>
2022-06-10 19:17:35 -0700

    build.rs

--- build.rs
+++ build.rs
@@ -4,10 +4,14 @@ use std::path::Path;
 
 fn main() {
 
-    Command::new("./include/lightning-2.1.3/configure")
+    env::set_current_dir("include/lightning-2.1.3").expect("unable to find include/lighting2.1.3 directory");
+    Command::new("./configure")
         .spawn().unwrap();
 
-    env::set_current_dir("include/lightning-2.1.3").expect("unable to find include/lighting2.1.3 directory");
     Command::new("make")
         .spawn().unwrap();
+
+    Command::new("make").args(&["install"])
+        .spawn().unwrap();
+
 }
